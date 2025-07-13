5f9fddd26d576327d25dbec3bd4da9b43f643078
xiaoyongcai <2283216402@qq.com>
2025-07-13 13:21:29 +0800

    FirstAdd

---
+++ normalTestModule/src/main/java/com/xiaoyongcai/io/normaltestmodule/NormalTestModuleApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoyongcai.io.normaltestmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalTestModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NormalTestModuleApplication.class, args);
+    }
+
+}
---
+++ normalTestModule/src/test/java/com/xiaoyongcai/io/normaltestmodule/NormalTestModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoyongcai.io.normaltestmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormalTestModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
